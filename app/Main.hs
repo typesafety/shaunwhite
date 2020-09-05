10c6784cdb67f5e591ccc3baf511242fc497edb8
Thomas Li <guslithd@student.gu.se>
2020-09-06 01:03:48 +0200

    Moved bot config related stuff to Bot.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,9 @@ module Main
        ( main
        ) where
 
-import Data.Aeson (decodeFileStrict')
 import Discord (def, runDiscord, RunDiscordOpts (..))
-import System.Directory (getXdgDirectory, XdgDirectory (XdgConfig))
-import System.FilePath ((</>))
-import System.IO.Error (catchIOError, IOError)
 
-import Bot (eventHandler, Cfg (..))
+import Bot (eventHandler, getConfig)
 
 
 main :: IO ()
@@ -33,24 +29,3 @@ main = do
 
     putStrLn "Finished."
 
-getConfig :: IO Cfg
-getConfig = do
-    cfgDir <- getXdgDirectory XdgConfig "shaunwhite"
-    let cfgLocation = cfgDir </> "config"
-    putTextLn $ "Reading config file from `" <> fromString cfgLocation <> "`"
-    mbyCfg <- catchIOError (decodeFileStrict' cfgLocation) handleRead
-    case mbyCfg of
-        Just c -> return c
-        Nothing -> do
-            putTextLn "Failed to read config file, using default config."
-            return defaultCfg
-  where
-    handleRead :: IOError -> IO (Maybe Cfg)
-    handleRead err = do
-        print err
-        return Nothing
-
-    defaultCfg :: Cfg
-    defaultCfg = Cfg
-        { cfgRequestableRoles = []
-        }
--- src/Bot.hs
+++ src/Bot.hs
@@ -1,17 +1,25 @@
 module Bot
        ( eventHandler
+
+       -- * Bot configuration
        , Cfg (..)
+       , defaultCfg
+       , getConfig
        ) where
 
-
-import Data.Aeson (FromJSON, parseJSON, withObject, (.:))
+import Data.Aeson (FromJSON, parseJSON, withObject, (.:), decodeFileStrict')
 import Discord (DiscordHandler)
 import Discord.Types (Event (MessageCreate))
+import System.Directory (getXdgDirectory, XdgDirectory (XdgConfig))
+import System.FilePath ((</>))
+import System.IO.Error (catchIOError, IOError)
 
 import Calls.Echo (cmdEcho)
 import Calls.Help (cmdHelp)
 import Commands (CmdEnv (..), Exec, Cmd (..), cmdFromMessage, isBotCommand)
 
+
+-- * Bot configuration
 
 data Cfg = Cfg
     { cfgRequestableRoles :: [Text]
@@ -20,6 +28,32 @@ data Cfg = Cfg
 instance FromJSON Cfg where
     parseJSON = withObject "Cfg" $ \ value -> Cfg
         <$> value .: "requestableRoles"
+
+defaultCfg :: Cfg
+defaultCfg = Cfg
+    { cfgRequestableRoles = []
+    }
+
+getConfigLocation :: IO FilePath
+getConfigLocation = getXdgDirectory XdgConfig "shaunwhite" <&> (</> "config")
+
+getConfig :: IO Cfg
+getConfig = do
+    cfgLocation <- getConfigLocation
+    putTextLn $ "Reading config file from `" <> fromString cfgLocation <> "`"
+    mbyCfg <- catchIOError (decodeFileStrict' cfgLocation) handleRead
+    case mbyCfg of
+        Just c -> return c
+        Nothing -> do
+            putTextLn "Failed to read config file, using default config."
+            return defaultCfg
+  where
+    handleRead :: IOError -> IO (Maybe Cfg)
+    handleRead err = do
+        print err
+        return Nothing
+
+-- * Event handler and command execution
 
 eventHandler :: Cfg -> Event -> DiscordHandler ()
 eventHandler cfg (MessageCreate msg) =
