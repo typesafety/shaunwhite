0a9a74a51d96564b2bf5280da244c3eb4ccb2deb
Thomas Li <guslithd@student.gu.se>
2021-08-23 15:44:48 +0200

    Restart with calamity

--- app/Main.hs
+++ app/Main.hs
@@ -1,58 +1,16 @@
 module Main
-       ( main
-       ) where
+    ( main
+    ) where
 
 import Relude
 
-import Discord (RunDiscordOpts (..))
-import System.Directory (doesFileExist, getHomeDirectory)
-import System.FilePath ((</>))
 
-import qualified Data.Text as T
-import qualified Discord
-
-import qualified Shaun.Bot as Bot
-import qualified Shaun.Env as Env
 
 
 main :: IO ()
-main = do
-    putTextLn "Starting shaunwhite..."
+main = putTextLn "Hello, world"
 
-    putTextLn "Reading bot token..."
-    botToken <- readFileText =<< getTokenPath
 
-    -- Set up initial environment.
-    initialEnv <- Env.initEnv
 
-    let discordOpts :: RunDiscordOpts
-        discordOpts =
-            Discord.def { discordToken   = botToken
-                        , discordOnEvent = Bot.eventHandler initialEnv
-                        }
 
-    putTextLn "Ready."
-    userFacingError <- Discord.runDiscord discordOpts
-    putTextLn userFacingError
 
-    putStrLn "Finished."
-
-  where
-    getTokenPath :: IO FilePath
-    getTokenPath = do
-        homeDir <- getHomeDirectory
-
-        let tokenLocations :: [FilePath]
-            tokenLocations = [homeDir </> ".shaunwhite.token", "token"]
-
-        searchFile tokenLocations
-      where
-        searchFile :: [FilePath] -> IO FilePath
-        searchFile []          = do
-            putTextLn "Could not find/read bot token."
-            exitFailure
-        searchFile (path : xs) = do
-            putTextLn $ "Attempting to read token from `" <> T.pack path <> "`..."
-            doesFileExist path >>= \case
-                True  -> return path
-                False -> searchFile xs
