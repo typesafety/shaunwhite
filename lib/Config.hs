512cb88b5d8f56c1ab2d2dc24d4000f7297e95c4
Thomas Li <ailoodee@gmail.com>
2021-09-22 23:47:18 +0200

    Config: refactor getToken

--- lib/Config.hs
+++ lib/Config.hs
@@ -28,15 +28,16 @@ The first token found in one of the following locations is used, in order:
 2. $XDG_CONFIG_HOME/shaunwhite/shaunwhite.token
 -}
 getToken :: Maybe FilePath -> IO Token
-getToken (Just explicitFp) = doesFileExist explicitFp >>= \case
-    True  -> makeBotToken <$> readFileText explicitFp
-    False -> fail $ "Token not found at: `" <> explicitFp <> "`"
-getToken Nothing = do
-    cfgDir <- getXdgDirectory XdgConfig configSubdir
-    token <- readFile $ cfgDir </> "shaunwhite" <.> "token"
-    pure . makeBotToken . fromString $ token
+getToken = \case
+    Just explicitFp -> doesFileExist explicitFp >>= \case
+        True  -> makeBotToken <$> readFileText explicitFp
+        False -> fail $ "Token not found at: `" <> explicitFp <> "`"
+    Nothing -> do
+        cfgDir <- getXdgDirectory XdgConfig configSubdir
+        token <- readFile $ cfgDir </> "shaunwhite" <.> "token"
+        pure . makeBotToken . fromString $ token
   where
     configSubdir = "shaunwhite"
 
-makeBotToken :: Text -> Token
-makeBotToken = BotToken . toLazy . T.strip
+    makeBotToken :: Text -> Token
+    makeBotToken = BotToken . toLazy . T.strip
